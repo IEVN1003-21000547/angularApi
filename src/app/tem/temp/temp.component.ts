eda9c8da9d4163a1284d24427876c1f018dd0d50
Victor Emiliano <vickb11k3@gmail.com>
2024-10-25 23:31:37 -0600

    25/10/2025

---
+++ src/app/tem/temp/temp.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { TemhComponent } from "../temh/temh.component";
+
+@Component({
+  selector: 'app-temp',
+  standalone: true,
+  imports: [TemhComponent],
+  templateUrl: './temp.component.html',
+  styles: ``
+})
+export class TempComponent {
+  title: string = 'Saludo desde el componente padre'; // Inicialización de la propiedad
+}
